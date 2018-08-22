013fae7e7fdaf79d46445c7d7441132b8454d929
qianchun <qianchun@huli.com>
2018-08-22 21:01:09 +0800

    init

--- src/main/java/com/wind/rabbitmq/queue/RabbitMqConfig.java
+++ src/main/java/com/wind/rabbitmq/queue/RabbitMqConfig.java
@@ -1,20 +1,15 @@
 package com.wind.rabbitmq.queue;
 
-import com.rabbitmq.client.Channel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.core.*;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
-import org.springframework.amqp.rabbit.config.DirectExchangeParser;
-import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.ChannelAwareMessageListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.support.ConsumerTagStrategy;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -75,23 +70,4 @@ public class RabbitMqConfig {
     public Binding binding() {
         return BindingBuilder.bind(userQueue()).to(directExchange()).with(ROUTING_USER);
     }
-
-//    @Bean
-//    public SimpleMessageListenerContainer messageContainer() {
-//        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer(connectionFactory());
-//        container.setQueues(userQueue());
-//        container.setExposeListenerChannel(true);
-//        container.setMaxConcurrentConsumers(1);
-//        container.setConcurrentConsumers(1);
-//        container.setAcknowledgeMode(AcknowledgeMode.MANUAL); //设置确认模式手工确认
-//        container.setMessageListener(new ChannelAwareMessageListener() {
-//            @Override
-//            public void onMessage(Message message, Channel channel) throws Exception {
-//                byte[] body = message.getBody();
-//                System.out.println("receive msg : " + new String(body));
-//                channel.basicAck(message.getMessageProperties().getDeliveryTag(), false); //确认消息成功消费
-//            }
-//        });
-//        return container;
-//    }
 }
